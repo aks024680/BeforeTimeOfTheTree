f1fcc5dc90cd66d160169b875fb91dfb136c2f7f
aks024680 <130197640+aks024680@users.noreply.github.com>
2025-01-04 19:35:30 +0800

    update 對話

---
+++ Assets/Scripts/Say/SayGameInput.cs
@@ -0,0 +1,19 @@
+﻿using Fungus;
+using UnityEngine;
+
+namespace BeforeTimeOfTheTree
+{
+
+    public class SayGameInput : MonoBehaviour
+    {
+        [SerializeField,Header("遊戲管理器")] private Flowchart fungusGM;
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            BasicInput();
+        }
+        protected virtual void BasicInput()
+        {
+            fungusGM.SendFungusMessage("遊戲基礎操作");
+        }
+    }
+}
